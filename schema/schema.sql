a859c6a2f70fa68b303a5896ec160209bf54c15b
Jeremy Tan <jeremy@jx.tan.fake>
2013-09-22 19:38:51 +0800

    manually created schema

---
+++ schema/schema.sql
@@ -0,0 +1,60 @@
+create table User
+(
+  userid int primary key,
+  username varchar(40) unique not null,
+  password varchar(40) not null,
+  contactid int,
+  registered date not null,
+  usertype int,
+  foreign key(contactid) references Contact(contactid)
+);
+
+create table Contact
+(
+  contactid int primary key,
+  lastname varchar(40) not null,
+  firstname varchar(40) not null,
+  email varchar(40) not null,
+  facebook varchar(40),
+  twitter varchar(40),
+  phone varchar(40),
+  fax varchar(40),
+  address varchar(255)  
+);
+
+create table Member
+(
+  memberid int primary key,
+  lastname varchar(40),
+  firstname varchar(40),
+  contactid int,
+  ownerid int,
+  interests varchar(255),
+  foreign key(ownerid) references User(userid),
+  foreign key(contactid) references Contact(contactid)
+);
+
+create table Club
+(
+  clubid int primary key,
+  name varchar(40) unique not null,
+  type varchar(40),
+  location varchar(100),
+  membercount int,
+  recruiting boolean,
+  description varchar(255),
+  contactid int,
+  ownerid int,
+  foreign key(ownerid) references User(userid),
+  foreign key(contactid) references Contact(contactid)
+);
+
+create table Membership
+(
+  memberid int,
+  clubid int,
+  joineddate date not null,
+  lastpaid date,
+  foreign key(memberid) references Member(memberid),
+  foreign key(clubid) references Club(clubid)
+);
