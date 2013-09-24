f15936e385628c9b147cca02f2f9305c475edf7c
Jeremy Tan <jtanx@users.noreply.github.com>
2013-09-24 11:01:07 +0800

    Update schema.sql

--- schema/schema.sql
+++ schema/schema.sql
@@ -2,45 +2,44 @@
 
 create table User
 (
-  userid int primary key,
+  userid int primary key not null,
   username varchar(40) unique not null,
   password varchar(40) not null,
-  contactid int,
+  lastname varchar(40),
+  firstname varchar(40),
+  contactid int not null,
   registered date not null,
-  usertype int,
+  usertype int not null,
   foreign key(contactid) references Contact(contactid)
 );
 
 create table Contact
 (
-  contactid int primary key,
-  --Consider moving lastname/firstname into the User/Member fields - doesn't make sense for Club
-  lastname varchar(40) not null,
-  firstname varchar(40) not null,
+  contactid int primary key not null,
   email varchar(40) not null,
+  address varchar(255),
   facebook varchar(40),
   twitter varchar(40),
   phone varchar(40),
-  fax varchar(40),
-  address varchar(255)  
+  fax varchar(40)
 );
 
 create table Member
 (
-  memberid int primary key,
-  contactid int,
-  ownerid int, --User that created this member
-  clubid int,
+  memberid int primary key not null,
+  lastname varchar(40) not null,
+  firstname varchar(40) not null,
+  contactid int not null,
+  ownerid int not null, --User that created this member
   interests varchar(255),
   foreign key(ownerid) references User(userid),
-  foreign key(clubid) references Club(clubid),
   foreign key(contactid) references Contact(contactid)
 );
 
 create table Club
 (
   clubid int primary key,
-  name varchar(40) unique not null,
+  name varchar(100) unique not null,
   type varchar(40),
   location varchar(100),
   membercount int,
@@ -54,8 +53,8 @@ create table Club
 
 create table Membership
 (
-  memberid int,
-  clubid int,
+  memberid int not null,
+  clubid int not null,
   joineddate date not null,
   lastpaid date,
   foreign key(memberid) references Member(memberid),
